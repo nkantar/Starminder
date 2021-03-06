61a82d54ed7b720fccb4b5e7a0cb044ce6566f3c
Nik Kantar <nik@nkantar.com>
2021-03-05 21:08:05 -0800

    Add first batch of files
    This hefty commit introduces the initial project scaffolding along with
    the first draft of models.
    
    Included:
    - Poetry config
    - generic Django project files
    - main app with relevant models
    - partial GitHub integration

---
+++ config/settings.py
@@ -0,0 +1,98 @@
+from os import getenv
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = getenv("STARMINDER_DJANGO_SECRET_KEY")
+
+DEBUG = bool(int(getenv("STARMINDER_DEBUG")))
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "main",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "starminder.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "starminder.wsgi.application"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = False
+
+USE_L10N = False
+
+USE_TZ = True
+
+STATIC_URL = "/static/"
+
+
+##################################################
+
+
+ADMIN_PREFIX = getenv("STARMINDER_ADMIN_PREFIX")
+
+ENCRYPTION_KEY = getenv("STARMINDER_ENCRYPTION_KEY")
---
+++ config/urls.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path(f"{settings.ADMIN_PREFIX}/admin/", admin.site.urls),
+    path("", include("starminder.main")),
+]
