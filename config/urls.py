957f10f94a862a51b20f56c826c81d550831d291
Nik Kantar <nik@nkantar.com>
2021-03-06 21:56:51 -0800

    Add partial GH auth implem. via django-allauth

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path(f"{settings.ADMIN_PREFIX}/admin/", admin.site.urls),
+    path(f"{settings.ADMIN_PREFIX}admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path("", include("starminder.main.urls")),
 ]
