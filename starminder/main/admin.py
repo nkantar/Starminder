b30e29b2014a21d4e7f8e214e09b17572a3b6a4c
Nik Kantar <nik@nkantar.com>
2021-03-06 21:58:27 -0800

    Fix formatting

--- starminder/main/admin.py
+++ starminder/main/admin.py
@@ -8,4 +8,5 @@ class ProfileAdmin(ModelAdmin):
     list_display = ("id", "username", "email", "day", "time", "number")
     list_display_links = ("id", "username", "email")
 
+
 admin.site.register(Profile, ProfileAdmin)
