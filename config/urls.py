58acc3369544cf575c6e2e8d0b63a4a59d1a8a4e
Nik Kantar <nik@nkantar.com>
2021-03-07 15:57:20 -0800

    Fix formatting

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path(f"{settings.ADMIN_PREFIX}admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
