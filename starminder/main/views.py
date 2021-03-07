622dc1935d986b50ae1c5012a82c8fd08887e672
Nik Kantar <nik@nkantar.com>
2021-03-07 15:59:04 -0800

    Add dashboard functionality

--- starminder/main/views.py
+++ starminder/main/views.py
@@ -1,6 +1,11 @@
+from datetime import time
+
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import TemplateView
+from django.views.generic import FormView, TemplateView
+
+from starminder.main.forms import ProfileForm
 
 
 class HomeView(TemplateView):
@@ -8,5 +13,35 @@ class HomeView(TemplateView):
 
 
 @method_decorator(login_required(login_url="/"), name="dispatch")
-class DashboardView(TemplateView):
+class DashboardView(FormView):
+    form_class = ProfileForm
     template_name = "dashboard.html"
+    success_url = reverse_lazy("dashboard")
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial.update(
+            {
+                "number": self.request.user.profile.number,
+                "day": self.request.user.profile.day,
+                "time": self.request.user.profile.time,
+                "html": self.request.user.profile.html,
+            }
+        )
+
+        return initial
+
+    def post(self, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            profile = self.request.user.profile
+            profile.number = int(form.data["number"])
+            profile.day = int(form.data["day"])
+            profile.time = time.fromisoformat(form.data["time"])
+            profile.html = form.data.get("html") == "on"
+            profile.save()
+
+            return self.form_valid(form)
+
+        else:
+            return self.form_invalid(form)
