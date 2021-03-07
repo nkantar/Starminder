0b394f5856545fe902a6841a078d39f5c9fb332b
Nik Kantar <nik@nkantar.com>
2021-03-07 15:59:04 -0800

    Add dashboard functionality

---
+++ starminder/main/forms.py
@@ -0,0 +1,29 @@
+from datetime import time as d_time
+
+from django.forms import (
+    BooleanField,
+    ChoiceField,
+    Form,
+    IntegerField,
+)
+
+
+DAYS = [
+    (0, "every day"),
+    (1, "Monday"),
+    (2, "Tuesday"),
+    (3, "Wednesday"),
+    (4, "Thursday"),
+    (5, "Friday"),
+    (6, "Saturday"),
+    (7, "Sunday"),
+]
+
+TIMES = [(d_time(hour), f"{hour}:00") for hour in range(0, 24)]
+
+
+class ProfileForm(Form):
+    number = IntegerField(label="How many stars", min_value=1)
+    day = ChoiceField(label="Day of week", choices=DAYS)
+    time = ChoiceField(label="Time of day (UTC)", choices=TIMES)
+    html = BooleanField(label="HTML email", required=False)
