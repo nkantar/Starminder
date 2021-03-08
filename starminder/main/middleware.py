3d0467b6c970956bcfde4749ae125c279e72d1af
Nik Kantar <nik@nkantar.com>
2021-03-07 16:43:46 -0800

    Add "email required" middleware and message

--- starminder/main/middleware.py
+++ starminder/main/middleware.py
@@ -1,9 +1,10 @@
 from django.conf import settings
+from django.contrib import messages
 
 from starminder.main.models import Profile
 
 
-class FooterStatsMiddleware:
+class StarminderMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
@@ -11,6 +12,15 @@ class FooterStatsMiddleware:
         response = self.get_response(request)
         return response
 
+
+class EmailRequiredMiddleware(StarminderMiddleware):
+    def process_template_response(self, request, response):
+        if request.user.is_authenticated and not request.user.email:
+            messages.add_message(request, messages.ERROR, "Email required!")
+        return response
+
+
+class FooterStatsMiddleware(StarminderMiddleware):
     def process_template_response(self, request, response):
         response.context_data["profile_count"] = Profile.objects.count()
         response.context_data["starminder_version"] = settings.STARMINDER_VERSION
