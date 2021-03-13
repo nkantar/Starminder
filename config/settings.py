ea7aa6e3bc6d11c3229d4a100cb1ec015e69bf60
Nik Kantar <nik@nkantar.com>
2021-03-12 23:30:28 -0800

    Disable staticfiles app

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
+    # "django.contrib.staticfiles",
     "django.contrib.sites",
     "allauth",
     "allauth.account",
