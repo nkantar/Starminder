e87c19344a8df562cf40b2098ca796153dc8ae7c
Nik Kantar <nik@nkantar.com>
2025-01-28 20:26:39 -0800

    Add Postgres for dev via Docker

--- starminder/settings.py
+++ starminder/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = "starminder.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": getenv("DB_NAME"),
+        "USER": getenv("DB_USER"),
+        "PASSWORD": getenv("DB_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": 5432,
     }
 }
 
