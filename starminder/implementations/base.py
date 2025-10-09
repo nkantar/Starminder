a65d915f51e96701189c65a613c5b7a28ee7bd39
Nik Kantar <nik@nkantar.com>
2025-10-08 23:09:03 -0700

    Add BaseImplementation + tests

---
+++ starminder/implementations/base.py
@@ -0,0 +1,41 @@
+import random
+from dataclasses import dataclass
+
+
+@dataclass
+class Entry:
+    owner: str
+    name: str
+    description: str | None
+    star_count: int
+    repo_url: str
+    project_url: str | None
+
+
+@dataclass
+class BaseImplementation:
+    access_token: str
+    max_entries: int = 5  # TODO eventually configurable
+
+    def retrieve_all_entries(self):
+        raise NotImplementedError
+
+    def populate_entries(self, entries: list) -> list:
+        return entries
+
+    def generate_entries(self):
+        entries = self.retrieve_all_entries()
+        sample_size = min(self.max_entries, len(entries))
+        sampled = random.sample(entries, sample_size)
+        populated = self.populate_entries(sampled)
+        return [
+            Entry(
+                owner=entry["owner"],
+                name=entry["name"],
+                description=entry["description"] if entry["description"] else None,
+                star_count=entry["star_count"],
+                repo_url=entry["repo_url"],
+                project_url=entry["project_url"] if entry["project_url"] else None,
+            )
+            for entry in populated
+        ]
