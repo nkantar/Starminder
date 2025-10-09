fcc92281c6f2cf890ed67823aa80ddf9b459cb31
Nik Kantar <nik@nkantar.com>
2025-10-08 21:46:58 -0700

    Add implementations app scaffolding
    This ties to

---
+++ starminder/implementations/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ImplementationsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "starminder.implementations"
--- starminder/settings.py
+++ starminder/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "starminder.core",
+    "starminder.implementations",
 ]
 
 MIDDLEWARE = [
