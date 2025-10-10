d0505c3e7723e0cb55c7e5c361ad5d1d4dc9a8a5
Nik Kantar <nik@nkantar.com>
2025-10-10 15:14:25 -0700

    Add config to user profile

--- starminder/core/models.py
+++ starminder/core/models.py
@@ -2,12 +2,15 @@ from typing import Any
 from uuid import uuid4
 
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db.models import (
     CASCADE,
     DateTimeField,
+    IntegerField,
     Manager,
     Model,
     OneToOneField,
+    PositiveIntegerField,
     UUIDField,
 )
 from django.db.models.signals import post_save
@@ -33,10 +36,37 @@ class CustomUser(AbstractUser):
 
 
 class UserProfile(TimestampedModel):
+    DAY = -1
+    MONDAY = 0
+    TUESDAY = 1
+    WEDNESDAY = 2
+    THURSDAY = 3
+    FRIDAY = 4
+    SATURDAY = 5
+    SUNDAY = 6
+
+    DAY_OF_WEEK_CHOICES = [
+        (DAY, "day"),
+        (MONDAY, "Monday"),
+        (TUESDAY, "Tuesday"),
+        (WEDNESDAY, "Wednesday"),
+        (THURSDAY, "Thursday"),
+        (FRIDAY, "Friday"),
+        (SATURDAY, "Saturday"),
+        (SUNDAY, "Sunday"),
+    ]
+
     objects: "Manager[UserProfile]"
 
     user = OneToOneField(CustomUser, on_delete=CASCADE, related_name="user_profile")
     feed_id = UUIDField(default=uuid4, unique=True)
+
+    max_entries = PositiveIntegerField(default=5)
+    day_of_week = IntegerField(choices=DAY_OF_WEEK_CHOICES, default=DAY)
+    hour_of_day = IntegerField(
+        default=0,
+        validators=[MinValueValidator(0), MaxValueValidator(23)],
+    )
 
     def __str__(self):
         return f"{self.user.username} (Profile)"
