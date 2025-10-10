067a0f48d97e3acf66a110211fcb7adf2fbbe924
Nik Kantar <nik@nkantar.com>
2025-10-09 18:19:58 -0700

    Add stub views + tests for homepage + dashboard

--- starminder/settings.py
+++ starminder/settings.py
@@ -137,3 +137,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+LOGIN_REDIRECT_URL = "/dashboard"
+LOGOUT_REDIRECT_URL = "/"
+ACCOUNT_LOGOUT_ON_GET = True
--- starminder/urls.py
+++ starminder/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("starminder.core.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
 ]
