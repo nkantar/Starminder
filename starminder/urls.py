278f481ab278a12a167d99f020c3586d5af9950b
Nik Kantar <nik@nkantar.com>
2025-10-09 19:43:59 -0700

    Add admin URL prefix

--- starminder/urls.py
+++ starminder/urls.py
@@ -1,8 +1,9 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path("", include("starminder.core.urls")),
-    path("admin/", admin.site.urls),
+    path(f"{settings.ADMIN_PREFIX}admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
 ]
