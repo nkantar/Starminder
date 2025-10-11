6e1fa605e84b152b51ebc1f46e24ca557078cbb1
Nik Kantar <nik@nkantar.com>
2025-10-10 17:56:56 -0700

    Add config to dashboard

---
+++ starminder/core/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+from starminder.core.models import UserProfile
+
+
+class UserProfileConfigForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ["max_entries", "day_of_week", "hour_of_day"]
+        widgets = {
+            "max_entries": forms.NumberInput(attrs={"min": 1}),
+            "day_of_week": forms.Select(),
+            "hour_of_day": forms.NumberInput(attrs={"min": 0, "max": 23}),
+        }
+        labels = {
+            "max_entries": "Maximum entries per post",
+            "day_of_week": "Day of week",
+            "hour_of_day": "Hour of day (0-23)",
+        }
