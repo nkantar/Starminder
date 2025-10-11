53cc606b541ca93face16a11736c858fe1359b75
Nik Kantar <nik@nkantar.com>
2025-10-10 17:56:56 -0700

    Add config to dashboard

--- starminder/core/views.py
+++ starminder/core/views.py
@@ -1,20 +1,36 @@
 from typing import Any
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import FormView, TemplateView
 from allauth.socialaccount.models import SocialAccount
+
+from starminder.core.forms import UserProfileConfigForm
 
 
 class HomepageView(TemplateView):
     template_name = "home.html"
 
 
-class DashboardView(LoginRequiredMixin, TemplateView):
+class DashboardView(LoginRequiredMixin, FormView):
     template_name = "dash.html"
+    form_class = UserProfileConfigForm
+    success_url = reverse_lazy("dashboard")
+
+    def get_form_kwargs(self) -> dict[str, Any]:
+        kwargs = super().get_form_kwargs()
+        kwargs["instance"] = self.request.user.user_profile
+        return kwargs
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context["socialaccount_list"] = SocialAccount.objects.filter(
             user=self.request.user
         )
+        context["user_profile"] = self.request.user.user_profile
         return context
+
+    def form_valid(self, form: UserProfileConfigForm) -> HttpResponse:
+        form.save()
+        return super().form_valid(form)
