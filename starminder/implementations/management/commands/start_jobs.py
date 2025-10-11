0ca2ea1518ad6abf835590691aabf6cb9d2b0f69
Nik Kantar <nik@nkantar.com>
2025-10-11 09:52:17 -0700

    Add missing types

--- starminder/implementations/management/commands/start_jobs.py
+++ starminder/implementations/management/commands/start_jobs.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from django.core.management.base import BaseCommand
 
 from starminder.implementations.jobs import start_jobs
@@ -6,5 +8,5 @@ from starminder.implementations.jobs import start_jobs
 class Command(BaseCommand):
     help = "Schedule content generation jobs for all applicable users"
 
-    def handle(self, *args, **options):
+    def handle(self, *args: Any, **options: Any) -> None:
         start_jobs()
