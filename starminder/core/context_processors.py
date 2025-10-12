e3cdb97602bd28f4e3a63ef2c836b29c94b8af08
Nik Kantar <nik@nkantar.com>
2025-10-12 10:58:08 -0700

    Clean up header and footer

--- starminder/core/context_processors.py
+++ starminder/core/context_processors.py
@@ -1,5 +1,11 @@
 from django.conf import settings
 
+from starminder.core.models import CustomUser
+
 
 def global_settings(request):
-    return {"VERSION": settings.STARMINDER_VERSION}
+    user_count = CustomUser.objects.filter(is_staff=False).count()
+    return {
+        "VERSION": settings.STARMINDER_VERSION,
+        "USER_COUNT": user_count,
+    }
