367790683729d3bc66562e6d95294ef523e980ef
Nik Kantar <nik@nkantar.com>
2025-10-13 21:20:40 -0700

    Add missing type hints

--- starminder/core/context_processors.py
+++ starminder/core/context_processors.py
@@ -1,9 +1,12 @@
+from typing import Any
+
 from django.conf import settings
+from django.http import HttpRequest
 
 from starminder.core.models import CustomUser
 
 
-def global_settings(request):
+def global_settings(request: HttpRequest) -> dict[str, Any]:
     user_count = CustomUser.objects.filter(is_staff=False).count()
     return {
         "VERSION": settings.STARMINDER_VERSION,
